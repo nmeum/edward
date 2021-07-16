34076cf995f1eed245a5607772c65ff0fcf72002
Sören Tempel <soeren+git@soeren-tempel.net>
2021-07-16 22:17:14 +0200

    tests: add tests for write command

--- tests/parse-cmd.scm
+++ tests/parse-cmd.scm
@@ -6,3 +6,18 @@
   (test-parse-cmd (list (make-addr '(current-line))) "  a")
   (test-parse-cmd (list (make-addr '(nth-line . 2342))) "2342    a")
   (test-parse-cmd (list (make-addr '(last-line . ()) '(42))) "$+42 a"))
+
+(test-group "write command"
+  (test-parse-cmd (list
+                    (list
+                      (make-addr '(nth-line . 1))
+                      #\,
+                      (make-addr '(last-line)))
+                    "") "w")
+
+  (test-parse-cmd (list
+                    (list
+                      (make-addr '(current-line))
+                      #\,
+                      (make-addr '(current-line) '(10)))
+                    "foobar") ".,.+10wfoobar"))
