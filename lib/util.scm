560296e1ec340b7f60fc4fc117fb3874376f0212
Sören Tempel <soeren+git@soeren-tempel.net>
2021-07-17 14:48:25 +0200

    util: add sublist procedure

--- lib/util.scm
+++ lib/util.scm
@@ -1,3 +1,11 @@
+;; Return sublist with start inclusive and end exclusive.
+
+(define (sublist lst start end)
+  (if (>= end start)
+    (error "invaldi sublist specification")
+    (let ((l (drop lst start)))
+      (drop-right l (- (length lst) end)))))
+
 ;; Like init from Haskell, returns everything except the last list element.
 
 (define (init lst)
