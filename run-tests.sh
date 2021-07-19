891c74d384d93c8b1620f52687d976034fa5fc48
Sören Tempel <soeren+git@soeren-tempel.net>
2021-07-19 13:12:40 +0200

    run-tests.sh: Initialize

---
+++ run-tests.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+export CHICKEN_REPOSITORY_PATH="$(pwd):${CHICKEN_REPOSITORY_PATH}"
+
+banner() {
+	printf "##\n# %s\n##\n\n" "${1}"
+}
+
+banner "Unit Tests"
+chicken-install -n -test
+
+banner "Integration Tests"
+./tests/integration/run.sh
