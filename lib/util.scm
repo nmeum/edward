22ace4db05bd66735a1b278bd7f83262623f330c
Sören Tempel <soeren+git@soeren-tempel.net>
2021-11-27 17:54:38 +0100

    util: remove init
    Now unused.

--- lib/util.scm
+++ lib/util.scm
@@ -119,15 +119,3 @@
   (count
     (lambda (x) (eqv? x #\newline))
     (string->list str)))
-
-;; Like init from Haskell, returns everything except the last list element.
-
-(define (init lst)
-  (define (%init lst)
-    (if (eqv? 1 (length lst))
-      '()
-      (cons (car lst) (%init (cdr lst)))))
-
-  (if (null? lst)
-    (error "empty list")
-    (%init lst)))
