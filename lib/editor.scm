b7e65008d75e3f4d84975513a81886ef0c916fab
Sören Tempel <soeren+git@soeren-tempel.net>
2021-11-28 20:17:58 +0100

    editor: Fix handling of underlying (chibi parse) source
    When updating the index the source might potentially need to be updated
    as well. This fixes several issues with ed input scripts which exceed
    the default (chibi parse) buffer size (256).

--- lib/editor.scm
+++ lib/editor.scm
@@ -48,7 +48,7 @@
   ;; Whether the prompt should be shown or hidden.
   (prompt? input-handler-prompt? input-handler-set-prompt!)
   ;; Parse stream used for the parser combinator.
-  (stream input-handler-stream)
+  (stream input-handler-stream input-handler-stream-set!)
   ;; Last index in parse stream.
   (index input-handler-index input-handler-index-set!))
 
@@ -60,25 +60,31 @@
       (make-parse-stream "stdin" (current-input-port))
       0)))
 
+(define (input-handler-state-set! handler source index)
+  (input-handler-stream-set! handler source)
+  (input-handler-index-set! handler index))
+
 (define (input-handler-parse handler f sk fk)
-  (define (index-of-next-line handler idx)
-    (let ((stream   (input-handler-stream handler))
-          (next-idx (inc idx)))
-      (if (eqv? (parse-stream-ref stream idx) #\newline)
-        next-idx ;; first index after newline
-        (index-of-next-line handler next-idx))))
+  (define (stream-next-line source idx)
+    (let* ((next-index  (parse-stream-next-index source idx))
+           (next-source (parse-stream-next-source source idx)))
+      (if (eqv? (parse-stream-ref source idx) #\newline)
+        (cons next-source next-index) ;; first index after newline
+        (stream-next-line
+          next-source
+          next-index))))
 
   (call-with-parse f
     (input-handler-stream handler)
     (input-handler-index handler)
     (lambda (r s i fk)
-      (input-handler-index-set! handler i)
+      (input-handler-state-set! handler s i)
       (sk (input-handler-line handler i) r))
     (lambda (s i reason)
-      (let ((l (input-handler-line handler i)))
-        (input-handler-index-set!
-          handler (index-of-next-line handler i))
-        (fk l reason)))))
+      (let ((line (input-handler-line handler i))
+            (next (stream-next-line (input-handler-stream handler) i)))
+        (input-handler-state-set! handler (car next) (cdr next))
+        (fk line reason)))))
 
 (define (input-handler-line handler index)
   ;; Surprisingly, (chibi parse) line numbers start at zero.
