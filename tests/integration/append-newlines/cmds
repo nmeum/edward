nmeum/edward

commit 25bac2b05d4043688c6903636c81396e45781212

Make more use of parse-token
Avoids backtracking via parse-repeat+.

lib/parse-util.scm
@@ -14,8 +14,7 @@
 
 (define parse-digits
   (parse-map
-    (parse-as-string
-      (parse-repeat+ (parse-char char-set:digit)))
+    (parse-token char-set:digit)
     string->number))
 
 (define (parse-default parser def)
@@ -80,7 +79,7 @@
   (parse-atomic
     (parse-map
       (parse-seq
-        (parse-as-string (parse-repeat (parse-not-char #\newline)))
+        (parse-token (lambda (x) (not (char=? x #\newline))))
         (parse-char #\newline))
       car)))
 

lib/cmd.scm
@@ -211,15 +211,17 @@
 (define parse-line-continuation
   (parse-map
     (parse-seq
-      (parse-as-string (parse-repeat+ (parse-not-char (char-set #\\ #\newline))))
+      (parse-token (lambda (x)
+                     (and
+                       (not (char=? x #\\))
+                       (not (char=? x #\newline)))))
       (parse-esc (parse-char #\newline)))
     (lambda (lst)
       (string-append (car lst) "\n"))))
 
 (define parse-last-line
   (parse-map
-    (parse-as-string
-      (parse-repeat+ (parse-not-char #\newline)))
+    (parse-token (lambda (x) (not (char=? x #\newline))))
     (lambda (str)
       (string-append str "\n"))))
 
@@ -309,10 +311,9 @@
       (parse-map
         (parse-seq
           (parse-string "!")
-          (parse-as-string
-            (parse-repeat+ (parse-not-char #\newline))))
+          (parse-token (lambda (x) (not (char=? x #\newline)))))
         (lambda (lst) (apply string-append lst)))
-      (parse-as-string (parse-repeat (parse-char char-set:graphic))))))
+      (parse-token char-set:graphic))))
 
 ;; Parses a command character followed by an optional file parameter.
 ;; The compontests **must** be separated by one or more <blank>

commit 00a145ac80932db6edd90cc7a0cfb6e94a2f3316

Fix handling of empty lines in parse-input-mode
Fixes regression introduced in 25bac2b05d4043688c6903636c81396e45781212
since parse-token is parse-repeat+, not parse-repeat. By utilizing
parse-or here we can continue to benefit from parse-token.

lib/parse-util.scm
@@ -77,11 +77,13 @@
 
 (define parse-line
   (parse-atomic
-    (parse-map
-      (parse-seq
-        (parse-token (lambda (x) (not (char=? x #\newline))))
-        (parse-char #\newline))
-      car)))
+    (parse-or
+      (parse-bind "" parse-newline) ;; empty line
+      (parse-map
+        (parse-seq
+          (parse-token (lambda (x) (not (char=? x #\newline))))
+          parse-newline)
+        car))))
 
 ;; Feed the result of the parser ctx to a single argument procedure f.
 ;; The procedure must then return a new parser which is executed

tests/integration/append-newlines/cmds
@@ -1,0 +1,15 @@
+a
+
+
+
+
+
+
+
+foo
+
+
+
+
+
+.